77279ebbaf0157315271fad10fb3cbd7be3c61d3
UtsavSingh29 <kutsavsingh>
2024-10-14 19:06:59 +0530

    addedthebasics

---
+++ proxy_server_with_cache.c
@@ -0,0 +1,24 @@
+#include "proxy_parse.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <pthreads.h>
+
+#define MAX_CLIENTS 10
+typedef struct cache_element cache_element;
+
+struct cache_element{
+    char*data;
+    int len;
+    char *url;
+    time_t lru_time_track;
+    cache_element*next;
+};
+
+cache_element*find(char*url);
+int add_cache_element(char*data,int size,char*url);
+void remove_cache_element();
+
+int port_number=8080;
+int proxy_socketId;
+pthread_t  tid[MAX_CLIENTS];
